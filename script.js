8d1937e86b3767dd87d0a5b6b0ed9861c1ca7f0f
JMD <jmdagenais@hotmail.ca>
2015-11-08 17:23:51 -0500

    first commit - add the files

---
+++ script.js
@@ -0,0 +1,15 @@
+var app = angular.module('computer', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider){
+  $routeProvider.
+  when('/main', {
+    templateUrl: 'main.html',
+    controller: 'MainCtrl'
+  }).
+  otherwise({redirectTo:'/main'});
+}])
+
+.controller('MainCtrl', ['$scope', function($scope){
+  console.log($scope);
+}]);
+
