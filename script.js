f4acedf60eb2059e2e2bd6d0c941e81b9d96949a
JMD <jmdagenais@hotmail.ca>
2015-11-21 14:50:45 -0500

    finish the 1st project

--- script.js
+++ script.js
@@ -6,10 +6,36 @@ var app = angular.module('computer', ['ngRoute'])
     templateUrl: 'main.html',
     controller: 'MainCtrl'
   }).
+  when('/about', {
+    templateUrl: 'about.html',
+    controller: 'MainCtrl'
+  }).
+  when('/services', {
+    templateUrl: 'services.html',
+    controller: 'ServicesCtrl'
+  }).
+  when('/contact', {
+    templateUrl: 'contact.html',
+    controller: 'ContactCtrl'
+  }).
   otherwise({redirectTo:'/main'});
 }])
 
-.controller('MainCtrl', ['$scope', function($scope){
-  console.log($scope);
+.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
+  $http.get('services.json').then(function(response){
+    $scope.services = response.data;
+  });
+}])
+
+.controller('ServicesCtrl', ['$scope', '$http', function($scope, $http){
+  $http.get('services.json').then(function(response){
+    $scope.services = response.data;
+  });
+}])
+
+.controller('ContactCtrl', ['$scope', '$http', function($scope, $http){
+  $http.get('locations.json').then(function(response){
+    $scope.locations = response.data;
+  });
 }]);
 
